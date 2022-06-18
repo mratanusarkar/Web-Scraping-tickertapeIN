e59915ac81e885b7a3b37c533fcddb16d3582a82
Atanu Sarkar <mratanusarkar@gmail.com>
2022-06-18 19:26:37 +0530

    bugfix: invalid output file path and name

--- Runner/Python/main.py
+++ Runner/Python/main.py
@@ -1,13 +1,19 @@
 from scraper_engine.list import TickerNames
 import json
 import time
+import os
 
 
-scraper = TickerNames(page_list=TickerNames.PAGE_LIST_ALL, include_type=TickerNames.TYPE_ALL)
+scraper = TickerNames(page_list=TickerNames.PAGE_LIST_ALL, include_type=TickerNames.TYPE_ALL, log=True)
 data = scraper.scrape()
 
 print("saving the data in json format...")
-filename = time.strftime("%Y-%m-%d_%H:%M:%S")
-with open("./data/Lists/" + filename + ".json", "w") as outfile:
+
+filename = "list_" + time.strftime("%Y_%m_%d_%H_%M_%S") + ".json"
+dirpath = os.path.abspath("./data/Lists/")
+filepath = os.path.join(dirpath, filename)
+
+with open(filepath, "w") as outfile:
     json.dump(data, outfile)
+
 print("completed!")
