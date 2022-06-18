efebb07651d0e5465322913fde0614f3fcad28af
Atanu Sarkar <mratanusarkar@gmail.com>
2022-06-18 17:54:37 +0530

    re-structured data out dir + added main.py

---
+++ Runner/Python/main.py
@@ -0,0 +1,13 @@
+from scraper_engine.list import TickerNames
+import json
+import time
+
+
+scraper = TickerNames(page_list=TickerNames.PAGE_LIST_ALL, include_type=TickerNames.TYPE_ALL, log=True)
+data = scraper.scrape()
+
+print("saving the data in json format...")
+filename = time.strftime("%Y-%m-%d_%H:%M:%S")
+with open("./data/Lists/" + filename + ".json", "w") as outfile:
+    json.dump(data, outfile)
+print("completed!")
