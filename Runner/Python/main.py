13be0884468ce40031c261c06dc0362d87f61630
Atanu Sarkar <mratanusarkar@gmail.com>
2022-06-18 22:02:29 +0530

    update main with new implementation

--- Runner/Python/main.py
+++ Runner/Python/main.py
@@ -1,19 +1,10 @@
 from tickertapein.scraper_engine.list import TickerNames
-import json
-import time
-import os
+from tickertapein.utils import DataSaver
 
 
 scraper = TickerNames(page_list=TickerNames.PAGE_LIST_ALL, include_type=TickerNames.TYPE_ALL, log=True)
+saver = DataSaver(scrape_type=DataSaver.SCRAPE_TYPE_LIST, log=True)
+
 data = scraper.scrape()
+saver.save(data)
 
-print("saving the data in json format...")
-
-filename = "list_" + time.strftime("%Y_%m_%d_%H_%M_%S") + ".json"
-dirpath = os.path.abspath("./tickertapein/data/Lists/")
-filepath = os.path.join(dirpath, filename)
-
-with open(filepath, "w") as outfile:
-    json.dump(data, outfile)
-
-print("completed!")
